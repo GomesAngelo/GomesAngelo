4689f6cb896dab178dcc1871acde3f3ce52ea4da
Ângelo Gomes Barros <109607809+GomesAngelo@users.noreply.github.com>
2023-05-31 12:47:52 -0300

    Add files via upload

---
+++ Angelo3D/assets/js/app.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('contato')
+const Cbtn = document.getElementById('coracao')
+const btnC = document.getElementById('cancelar')
+
+
+Cbtn.addEventListener("click", ()=>{
+
+})
+
+btn.addEventListener("click",()=>{
+    const modalup = document.querySelector('#modal')
+    
+    console.log(modalup)
+})
